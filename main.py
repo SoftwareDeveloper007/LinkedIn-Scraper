cfc652964796dae7de249ff0a69c37ece1ab7c1c
Taaniel Magi <https://github.com/YellowBackCorn>
2017-09-24 21:32:03 -0700

    2nd Commit

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class mainScraper():
 
         self.search_word = search_word
         self.output_data = []
+        self.base_url = ''
 
     def startScraping(self):
 
@@ -26,12 +27,12 @@ class mainScraper():
         self.driver.get('https://www.linkedin.com/m/login/')
         self.driver.switch_to.frame(self.driver.find_element_by_tag_name("iframe"))
 
-
+        '''
         signin = WebDriverWait(self.driver, 200).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, "p.signin-link > a"))
         )
         signin.click()
-
+        '''
         id = WebDriverWait(self.driver, 200).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, "input#session_key-login"))
         )
@@ -89,8 +90,141 @@ class mainScraper():
         current_company.click()
 
         self.driver.find_element_by_css_selector('li.search-facet.search-facet--current-company').find_elements_by_tag_name('li')[0].click()
-        print(self.driver.find_element_by_css_selector(
-            'li.search-facet.search-facet--current-company').find_elements_by_tag_name('li')[0].text)
+
+        self.base_url = self.driver.current_url
+
+        page_index = 0
+        while 1:
+            if 'No results found.' not in self.driver.find_element_by_tag_name('html').text.strip():
+                page_index += 1
+            else:
+                break
+
+            if page_index > 1:
+                self.driver.get(self.base_url + '&page={}'.format(page_index))
+
+            rows = WebDriverWait(self.driver, 200).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "li.search-result")))
+
+            for i, row in enumerate(rows):
+                line = row.find_element_by_css_selector('span.name.actor-name')
+                if 'LinkedIn Member' not in line.text.strip():
+                    line.click()
+                else:
+                    continue
+
+                try:
+                    headline_row = WebDriverWait(self.driver, 200).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.pv-top-card-section__body")))
+                    try:
+                        headline_row.find_element_by_css_selector('button.pv-top-card-section__summary-toggle-button').click()
+                    except:
+                        pass
+
+                    try:
+                        full_name = headline_row.find_element_by_class_name('pv-top-card-section__name').text.strip()
+                    except:
+                        full_name = ''
+                    try:
+                        current_title = headline_row.find_element_by_class_name('pv-top-card-section__headline').text.strip()
+                    except:
+                        current_title = ''
+                    try:
+                        location = headline_row.find_element_by_class_name('pv-top-card-section__location').text.strip()
+                    except:
+                        location = ''
+
+                    try:
+                        company_desc = headline_row.find_element_by_class_name('pv-top-card-section__summary-text').text.strip()
+                    except:
+                        company_desc = ''
+
+                except:
+                    full_name = ''
+                    current_title = ''
+                    location = ''
+                    company_desc = ''
+
+                experience = []
+                for i in range(10):
+                    experience.append(['', '', '', ''])
+                try:
+                    experience_row = self.driver.find_element_by_css_selector('section.pv-profile-section.experience-section.ember-view')
+                    try:
+                        experience_row.find_element_by_css_selector('button.pv-profile-section__see-more-inline').click()
+                    except:
+                        pass
+
+                    sub_experiences = WebDriverWait(experience_row, 200).until(
+                        EC.presence_of_all_elements_located((By.CSS_SELECTOR, "ul.pv-profile-section__section-info > li"))
+                    )
+
+
+                    for i, elm in enumerate(sub_experiences):
+                        try:
+                            job_title = elm.find_element_by_tag_name('h3').text.strip()
+                        except:
+                            job_title = ''
+
+                        try:
+                            company_name = elm.find_elements_by_tag_name('h4')[0].text.strip()
+                        except:
+                            company_name = ''
+
+                        try:
+                            period = elm.find_elements_by_tag_name('h4')[1].text.strip() + ': ', elm.find_elements_by_tag_name('h4')[2].text.strip()
+                        except:
+                            period = ''
+
+                        try:
+                            geolocation = elm.find_elements_by_tag_name('h4')[3].text.strip()
+                        except:
+                            geolocation = ''
+
+                        experience[i] = [job_title, company_name, period, geolocation]
+
+                except:
+                    pass
+
+                education = []
+                for i in range(5):
+                    education.append(['', '', ''])
+
+                try:
+                    education_row = self.driver.find_element_by_css_selector(
+                        'section.pv-profile-section.education-section.ember-view')
+                    try:
+                        education_row.find_element_by_css_selector(
+                            'button.pv-profile-section__see-more-inline').click()
+                    except:
+                        pass
+
+                    sub_educations = education_row.find_elements_by_css_selector(
+                        'ul.pv-profile-section__section-info > li')
+
+                    for i, elm in enumerate(sub_educations):
+                        try:
+                            edu_institution = elm.find_element_by_css_selector('div.pv-entity__degree-info > h3').text.strip()
+                        except:
+                            edu_institution = ''
+
+                        try:
+                            degree = elm.find_element_by_css_selector('div.pv-entity__degree-info').text.strip().replace(edu_institution, '').strip()
+                        except:
+                            degree = ''
+
+                        try:
+                            date = elm.find_element_by_css_selector('p.pv-entity__dates').text.strip()
+                        except:
+                            date = ''
+
+                        education[i] = [edu_institution, degree, date]
+
+
+                except:
+                    pass
+                print('OK')
+
+
+
         print('OK')
 
 if __name__ == '__main__':
